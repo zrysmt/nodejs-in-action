a46c78d5f2e084d7b3259b6b51007ed59cf4e74d
zrysmt <742311374@qq.com>
2017-06-15 20:22:35 +0800

    update

--- 7/directory.js
+++ 7/directory.js
@@ -1,10 +1,16 @@
 var connect = require('connect');
 var express    = require('express')
 var path = require('path');
+var serveStatic  = require('serve-static');
 var serveIndex  = require('serve-index');
 
 var app = connect()
 // var app = express()
-	.use('/limit',serveIndex(path.join(__dirname,'limit'), {'icons': true}))  //文件夹/nodejs/code/7/limit
+	.use('/',serveIndex(__dirname))  
+	.use('/',serveStatic(__dirname))  
+	.use('/limit',serveIndex(path.join(__dirname,'limit')))  
+	.use('/limit',serveStatic(path.join(__dirname,'limit')))
 
 app.listen(3000);
+
+//http://localhost:3000
